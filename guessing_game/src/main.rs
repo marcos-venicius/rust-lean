e8c56c31a7ebb57f4a78c4554a768e0e2bd41b2c
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-11-23 22:44:20 -0300

    little refactoring

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,38 +4,34 @@ use std::{cmp::Ordering, io, process};
 fn main() {
     println!("GUESSING GAME\n");
 
-    // generate a random number between 0 and 100
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
+    let mut guess: String;
     let mut attempts: u16 = 0;
 
     loop {
-        let mut guess = String::new();
+        guess = String::new();
 
-        if attempts > 0 {
-            println!("[{attempts}] attempts");
+        if attempts > 1 {
+            println!("{attempts} attempts");
         }
 
-        println!("your guess: ");
+        println!("guess: ");
 
-        // get user niput
         io::stdin()
             .read_line(&mut guess)
             .expect("cannot read guess");
 
-        // clear output
         process::Command::new("clear").status().unwrap();
 
-        // parse the guess string text to u32
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("please inform a valid number");
+                println!("[!] invalid guess");
                 continue;
             }
         };
 
-        // compare values
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Equal => {
