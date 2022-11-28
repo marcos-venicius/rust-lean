b77910bd501ef26a017401165e06c69e715105b8
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-11-28 19:17:45 -0300

    add lables to loops

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,16 +1,19 @@
 mod loops_returning_values;
+mod loops_label;
 mod nested_if;
 
 pub enum Program {
     NestedIf,
     LoopsReturningValues,
+    LoopsLabel
 }
 
-const PROGRAM_RUNNING: Program = Program::LoopsReturningValues;
+const PROGRAM_RUNNING: Program = Program::LoopsLabel;
 
 fn main() {
     match PROGRAM_RUNNING {
         Program::NestedIf => nested_if::new(),
         Program::LoopsReturningValues => loops_returning_values::new(),
+        Program::LoopsLabel => loops_label::new(),
     }
 }
